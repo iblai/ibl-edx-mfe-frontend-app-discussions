a09ad2772bb90d79994b11f22d808cb241915e1d
gipsbrian <gipsbrian@gmail.com>
2024-10-12 21:26:09 +0000

    udpate to webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,21 +1,8 @@
-const getBaseConfig = require('@edx/frontend-build/lib/getBaseConfig');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const getBaseConfig = require('@openedx/frontend-build/lib/webpack.config.js');
 
-const baseConfig = getBaseConfig('webpack');
+const config = getBaseConfig('webpack-prod');
 
-const { plugins } = baseConfig;
-const htmlWebpackPluginIndex = plugins.findIndex(plugin => plugin.constructor && plugin.constructor.name === 'HtmlWebpackPlugin');
-if (htmlWebpackPluginIndex !== -1) {
-  plugins[htmlWebpackPluginIndex] = new HtmlWebpackPlugin({
-    inject: true, // Appends script tags linking to the webpack bundles at the end of the body
-    template: path.resolve(process.cwd(), 'public/index.html'),
-    FAVICON_URL: process.env.FAVICON_URL || null,
-    OPTIMIZELY_PROJECT_ID: process.env.OPTIMIZELY_PROJECT_ID || null,
-    NODE_ENV: process.env.NODE_ENV || null,
-    MFE_STATIC_CSS_DOMAIN: process.env.MFE_STATIC_CSS_DOMAIN || null,
-  });
-}
+// Add any custom configurations here
 
-baseConfig.plugins = plugins;
-module.exports = baseConfig;
+module.exports = config;
